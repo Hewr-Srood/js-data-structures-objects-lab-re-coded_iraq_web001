ea3a68e9c9d8e4cbbfaa7a347506f8499d374555
Hewr Srood <hewr_s@outlook.com>
2020-06-02 17:26:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ function updateDriverWithKeyAndValue(driver, key, value){
 return obj;
 }
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-
+  
   let obj=  Object.assign(driver,{[key]:value});
   return obj;
 
 }
 function deleteFromDriverByKey(driver, key){
+
   let newDriver = Object.assign({}, driver);
-  delete newdriver[key];
-  return newdriver;
+
+  delete newDriver[key];
+return newDriver;
 }
 function destructivelyDeleteFromDriverByKey(driver, key){
+
   delete driver[key];
   return driver;
 }
